c5cbc4c7a3c92b5a2665307cfff5f6a9cd19b134
Abastecimiento-milicic <david.benitez@milicic.com.ar>
2026-07-07 15:40:29 -0300

    Update last-update.js

--- last-update.js
+++ last-update.js
@@ -1,7 +1,7 @@
 // Este archivo permite modificar la fecha y versión de actualización manualmente para todas las pestañas.
 // Modifique los valores aquí abajo y se verán reflejados en todo el sitio.
 
-const FECHA_ACTUALIZACION = "03/07/2026";
+const FECHA_ACTUALIZACION = "07/07/2026";
 const VERSION_ACTUALIZACION = "v. 20260623";
 
 // Compartir las variables globalmente
