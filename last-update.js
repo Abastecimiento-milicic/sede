767e3f763419650cee56ee4746cf468a57467d51
Abastecimiento-milicic <david.benitez@milicic.com.ar>
2026-07-14 16:32:03 -0300

    Add files via upload

---
+++ last-update.js
@@ -0,0 +1,30 @@
+// Este archivo permite modificar la fecha y versión de actualización manualmente para todas las pestañas.
+// Modifique los valores aquí abajo y se verán reflejados en todo el sitio.
+
+const FECHA_ACTUALIZACION = "07/07/2026";
+const VERSION_ACTUALIZACION = "v. 20260623";
+
+// Utilizar localStorage para mantener la sesión de caché sincronizada entre pestañas y navegaciones
+let cacheBuster = localStorage.getItem("APP_CACHE_BUSTER");
+if (!cacheBuster) {
+    cacheBuster = Date.now().toString(36);
+    localStorage.setItem("APP_CACHE_BUSTER", cacheBuster);
+}
+
+// Compartir las variables globalmente
+window.FECHA_ACTUALIZACION = FECHA_ACTUALIZACION;
+window.VERSION_ACTUALIZACION = VERSION_ACTUALIZACION;
+window.CACHE_BUSTER = cacheBuster;
+
+window.forceRefreshData = function() {
+    // Forzamos un nuevo buster en el almacenamiento local
+    localStorage.setItem("APP_CACHE_BUSTER", Date.now().toString(36));
+    
+    if (window.clearDataCache) {
+        window.clearDataCache().then(() => {
+            window.location.reload(true);
+        });
+    } else {
+        window.location.reload(true);
+    }
+};
